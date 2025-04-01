8537f42e3350ac99871d6e3cd209026fea9180c5
Marlon Quilante <marlonquilante@gmail.com>
2025-04-01 20:27:25 -0300

    Implementado a lógica do oponente no programa

--- JogoDosDados.ConsoleApp/Program.cs
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -9,11 +9,14 @@
             while (true)
             {
                 int posicaoUsuario = 0;
+                int posicaoComputador = 0;
                 bool jogoEstaEmAndamento = true;
 
                 while (jogoEstaEmAndamento)
                 {
-                    ExibirCabecalho();
+                    //Turno do usuário
+
+                    ExibirCabecalho("Usuário");
 
                     int resultado = LancarDado();
 
@@ -24,11 +27,42 @@
                     if (posicaoUsuario >= limiteLinhaChegada)
                     {
                         Console.WriteLine("Parabéns, você alcançou a linha de chegada!");
+                        Console.Write("\nPressione ENTER para continuar...");
+                        Console.ReadLine();
+
                         jogoEstaEmAndamento = false;
+                        continue;
                     }
                     else
                     {
                         Console.WriteLine($"O jogador está na posição: {posicaoUsuario} de {limiteLinhaChegada}");
+                    }
+
+                    Console.Write("\nPressione ENTER para continuar...");
+                    Console.ReadLine();
+
+                    //Turno do computador
+
+                    ExibirCabecalho("Computador");
+
+                    int resultadoComputador = LancarDado();
+
+                    ExibirResultadoSorteio(resultadoComputador);
+
+                    posicaoComputador += resultadoComputador;
+
+                    if (posicaoComputador >= limiteLinhaChegada)
+                    {
+                        Console.WriteLine("Que pena, o computador alcançou a linha de chegada!");
+
+                        Console.Write("\nPressione ENTER para continuar...");
+                        Console.ReadLine();
+                        jogoEstaEmAndamento = false;
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"O computador está na posição: {posicaoComputador} de {limiteLinhaChegada}");
                     }
 
                     Console.Write("\nPressione ENTER para continuar...");
@@ -42,15 +76,20 @@
             }
         }
 
-        static void ExibirCabecalho()
+        static void ExibirCabecalho(string nomeJogador)
         {
             Console.Clear();
             Console.WriteLine("------------------------------------------");
             Console.WriteLine("Jogo dos Dados");
             Console.WriteLine("------------------------------------------");
+            Console.WriteLine($"Turno do: {nomeJogador}");
+            Console.WriteLine("------------------------------------------");
 
-            Console.WriteLine("Pressione ENTER para lançar o dado...");
-            Console.ReadLine();
+            if (nomeJogador != "Computador")
+            {
+                Console.Write("Pressione ENTER para lançar o dado...");
+                Console.ReadLine();
+            }
         }
 
         static int LancarDado()
