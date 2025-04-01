f567e38de1bc7a19f1f1b44aad6f14cd027c5dd7
Marlon Quilante <marlonquilante@gmail.com>
2025-04-01 19:48:17 -0300

    Implementado estrutura básica do programa

--- JogoDosDados.ConsoleApp/Program.cs
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                ExibirCabecalho();
+
+                int resultado = LancarDado();
+
+                ExibirResultadoSorteio(resultado);
+
+                string opcaoContinuar = ExibirMenuContinuar();
+
+                if (opcaoContinuar != "S")
+                    break;
+            }
+        }
+
+        static void ExibirCabecalho()
+        {
+            Console.Clear();
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Jogo dos Dados");
+            Console.WriteLine("------------------------------------------");
+
+            Console.WriteLine("Pressione ENTER para lançar o dado...");
+            Console.ReadLine();
+        }
+
+        static int LancarDado()
+        {
+            Random geradorDeNumeros = new Random();
+
+            return geradorDeNumeros.Next(1, 7);
+        }
+
+        static void ExibirResultadoSorteio(int resultado)
+        {
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine($"O valor sorteado foi: {resultado}");
+            Console.WriteLine("------------------------------------------");            
+        }
+
+        static string ExibirMenuContinuar()
+        {
+            Console.Write("Deseja continuar? (S/N) ");
+            return Console.ReadLine()!.ToUpper();
         }
     }
 }
