3f51e4bf32b87d6952407129236d9383ac790d2c
Marlon Quilante <marlonquilante@gmail.com>
2025-04-15 20:53:35 -0300

    Implementada a classe Jogador para que não haja repetição de código entre usuário e computador

--- JogoDosDados.ConsoleApp/Jogador.cs
+++ JogoDosDados.ConsoleApp/Jogador.cs
@@ -1,35 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JogoDosDados.ConsoleApp
+﻿namespace JogoDosDados.ConsoleApp
 {
     internal class Jogador
     {
-        int posicao = 0;
+        public int posicao = 0;
+        public string nome = "";
 
         public void ExecutarRodada()
         {
-            ExibirCabecalho("Usuário");
+            ExibirCabecalho();
             int resultadoUsuario = LancarDado();
             ExibirResultadoSorteio(resultadoUsuario);
             posicao = AvancarCasa(posicao, resultadoUsuario);
-
-            if (Venceu())
-            {
-                Console.WriteLine("Parabéns, você alcançou a linha de chegada!");
-                Console.Write("\nPressione ENTER para continuar...");
-                Console.ReadLine();
-                Program.jogoEstaEmAndamento = false;
-            }
-            else
-            {
-                Console.WriteLine($"Você está na posição: {posicao} de {Program.limiteLinhaChegada}");
-                Console.Write("\nPressione ENTER para continuar...");
-                Console.ReadLine();
-            }
+            ExibirPosicao();
         }
 
         public int AvancarCasa(int posicaoUsuario, int resultadoUsuario)
@@ -79,15 +61,21 @@ namespace JogoDosDados.ConsoleApp
             }
         }
 
-        void ExibirCabecalho(string nomeJogador)
+        public void MensagemVitoria()
+        {
+            Console.Clear();
+            Console.WriteLine($"Vitória do {nome}!");
+        }
+
+        private void ExibirCabecalho()
         {
             Console.Clear();
             Console.WriteLine("------------------------------------------");
             Console.WriteLine("Jogo dos Dados");
             Console.WriteLine("------------------------------------------");
-            Console.WriteLine($"Turno do: {nomeJogador}");
+            Console.WriteLine($"Turno do: {nome}");
 
-            if (nomeJogador != "Computador")
+            if (nome != "Computador")
             {
                 Console.WriteLine("------------------------------------------");
                 Console.Write("\nPressione ENTER para lançar o dado...");
@@ -95,18 +83,25 @@ namespace JogoDosDados.ConsoleApp
             }
         }
 
-        int LancarDado()
+        private int LancarDado()
         {
             Random geradorDeNumeros = new Random();
 
             return geradorDeNumeros.Next(1, 7);
         }
 
-        void ExibirResultadoSorteio(int resultado)
+        private void ExibirResultadoSorteio(int resultado)
         {
             Console.WriteLine("------------------------------------------");
             Console.WriteLine($"O valor sorteado foi: {resultado}");
             Console.WriteLine("------------------------------------------");
+        }
+
+        public void ExibirPosicao()
+        {
+            Console.WriteLine($"{nome} na posição: {posicao} de {Program.limiteLinhaChegada}");
+            Console.Write("\nPressione ENTER para continuar...");
+            Console.ReadLine();
         }
     }
 }
--- JogoDosDados.ConsoleApp/Program.cs
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -9,19 +9,26 @@
         {
             while (true)
             {
+                Jogador usuario = new Jogador();
+                usuario.nome = "Usuário";
+                Jogador computador = new Jogador();
+                computador.nome = "Computador";
+
                 while (jogoEstaEmAndamento)
                 {
-                    Usuario.ExecutarRodada();
+                    usuario.ExecutarRodada();
 
-                    if (Usuario.Venceu())
+                    if (usuario.Venceu())
                     {
+                        usuario.MensagemVitoria();
                         break;
                     }
 
-                    Computador.ExecutarRodada();
+                    computador.ExecutarRodada();
 
-                    if (Computador.Venceu())
+                    if (computador.Venceu())
                     {
+                        computador.MensagemVitoria();
                         break;
                     }
                 }
@@ -33,39 +40,9 @@
             }
         }
 
-        static void ExibirCabecalho(string nomeJogador)
-        {
-            Console.Clear();
-            Console.WriteLine("------------------------------------------");
-            Console.WriteLine("Jogo dos Dados");
-            Console.WriteLine("------------------------------------------");
-            Console.WriteLine($"Turno do: {nomeJogador}");
-
-            if (nomeJogador != "Computador")
-            {
-                Console.WriteLine("------------------------------------------");
-                Console.Write("\nPressione ENTER para lançar o dado...");
-                Console.ReadLine();
-            }
-        }
-
-        static int LancarDado()
-        {
-            Random geradorDeNumeros = new Random();
-
-            return geradorDeNumeros.Next(1, 7);
-        }
-
-        static void ExibirResultadoSorteio(int resultado)
-        {
-            Console.WriteLine("------------------------------------------");
-            Console.WriteLine($"O valor sorteado foi: {resultado}");
-            Console.WriteLine("------------------------------------------");
-        }
-
         static string ExibirMenuContinuar()
         {
-            Console.Write("\nDeseja continuar? (S/N) ");
+            Console.Write("\nDeseja jogar novamente? (S/N) ");
             return Console.ReadLine()!.ToUpper();
         }
     }
